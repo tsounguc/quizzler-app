9a3b731725719e5d1eb136e10503feb27d08813d
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-10-30 21:04:35 -0400

    Class based Tkinter UI

--- ui.py
+++ ui.py
@@ -1,2 +1,33 @@
 THEME_COLOR = "#375362"
+from tkinter import *
 
+
+class QuizInterface:
+    def __init__(self, quiz):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text="Score: 0", font=("Arial", 12, "normal"), bg=THEME_COLOR, foreground="White")
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, highlightthickness=0)
+
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            text="Hello",
+            font=("Arial", 20, "italic"),
+            fill=THEME_COLOR
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button.grid(row=2, column=0)
+
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button.grid(row=2, column=1)
+
+        self.window.mainloop()
